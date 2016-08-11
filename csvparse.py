07768fc2c2865fe0bb98379bbf3f99ebb7da49a9
asdfMaciej <maciej.kaszkowiak@gmail.com>
2016-08-12 01:47:22 +0200

    General revamp of the code, added argparse.

--- csvparse.py
+++ csvparse.py
@@ -1,8 +1,9 @@
 import csv
 import datetime
+import argparse
 
 
-class Mainclass:
+class MainClass:
 	def __init__(self):
 		with open('pokemony.txt', 'r') as poksy_f:
 			self.poksy = poksy_f.readlines()
@@ -13,7 +14,7 @@ class Mainclass:
 		date_object = date_object - datetime.timedelta(minutes=15)
 		return date_object.strftime('%H:%M:%S')
 
-	def parse_nest(self, place, wanted_pokemon='', nest=False):
+	def parse_pokemon(self, place, wanted_pokemon='', nest=0):
 		place_array = []
 		place_array.append(place[0][0])
 		place_array.append(place[0][1])
@@ -56,50 +57,86 @@ class Mainclass:
 		place_array.append(disappear_str)
 
 		if place_array[2]:
-			if nest:
-				if int(poke_dict[wanted_pokemon]) > 1:
-					return place_array
-			else:
+			if int(poke_dict[wanted_pokemon]) >= nest:
 				return place_array
 		
 		return None
 
-csvparse = Mainclass()
+	def parse_csv(self, filename):
+		csvfile = open(filename, 'rb')
+		reader = csv.reader(csvfile, delimiter=',')
+		csvarray = []
+		for row in reader:
+			csvarray.append(row)
 
-csvfile = open('testestst.csv', 'rb')
-reader = csv.reader(csvfile, delimiter=',')
-csvarray = []
-for row in reader:
-	csvarray.append(row)
+		dict_ = {}
+		for row in csvarray:
+			if row[1] not in dict_:
+				dict_[row[1]] = []
+			add_ = []
+			add_.append(row[3])  # latitude
+			add_.append(row[4])  # longitude
+			add_.append(row[2])  # pokemon
+			add_.append(row[5])  # disappear time
+			add_.append(row[0])  # encounter id
+			dict_[row[1]].append(add_)
 
-dict_ = {}
-for row in csvarray:
-	if row[1] not in dict_:
-		dict_[row[1]] = []
-	add_ = []
-	add_.append(row[3])  # latitude
-	add_.append(row[4])  # longitude
-	add_.append(row[2])  # pokemon
-	add_.append(row[5])  # disappear time
-	add_.append(row[0])  # encounter id
-	dict_[row[1]].append(add_)
+		return dict_
 
-sorted_dict = [['latitude', 'longitude', 'pokemon', 'appear_times', 'disappear_times']]
-for useless, place in dict_.iteritems():
-	place_array = csvparse.parse_nest(place, wanted_pokemon='Drowzee', nest=True)
-	if place_array:
-		sorted_dict.append(place_array)
+class InputHandler:
+	def __init__(self):
+		self.parser = argparse.ArgumentParser()
+		self.parser.add_argument("input", help="The input csv file.")
+		self.parser.add_argument(
+			"-o", "--output",
+			help="The output csv file, defaults to pokemon.csv.",
+			default="pokemon.csv"
+			)
+		self.parser.add_argument(
+			"-n", "--nest",
+			help="Output only nests with specified amount of pokemon.",
+			type=int, default=0
+		)
+		self.parser.add_argument(
+			"-t", "--type",
+			help="Output only certain pokemon, for ex. Zubat.",
+			default=""
+		)
 
-end_result = ""
-for line in sorted_dict:
-	end_result += ','.join(line)+'\n'
+	def parse_args(self):
+		return self.parser.parse_args()
 
-koncowe = open('drowzee.csv', 'w')
-koncowe.write(end_result)
-koncowe.close()
+class MainRunner:  
+	def __init__(self):
+		self.mainclass = MainClass()
+		self.inputhandler = InputHandler()
 
+	def run(self):
+		args = self.inputhandler.parse_args()
+		_input = args.input
+		_output = args.output
+		_type = args.type
+		if _output == 'pokemon.csv' and _type:
+			_output = _type+'.csv'
+		_nest = args.nest
 
+ 		dict_ = self.mainclass.parse_csv(_input)
+		sorted_dict = [['latitude', 'longitude', 'pokemon', 'appear_times', 'disappear_times']]
+		for useless, place in dict_.iteritems():
+			place_array = self.mainclass.parse_pokemon(
+				place, wanted_pokemon=_type, nest=_nest
+			)
+			if place_array:
+				sorted_dict.append(place_array)
 
+		end_result = ""
+		for line in sorted_dict:
+			end_result += ','.join(line)+'\n'
 
-#for x in sorted_dict:
-#	print x + ', spawns: ' + str(dict_[x])
+		koncowe = open(_output, 'w')
+		koncowe.write(end_result)
+		koncowe.close()
+
+if __name__ == '__main__':
+	main = MainRunner()
+	main.run()
